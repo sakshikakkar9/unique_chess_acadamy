17c8eb84632bafea50debc90684e9fec9a0b9651
sakshikakkar9 <sakahikakkar111@gmail.com>
2026-05-01 11:46:49 +0530

    upadate index.js

--- server/index.js
+++ server/index.js
@@ -88,7 +88,7 @@ app.use('/api/courses', courseRoutes);
 // ------------------------------------------------------
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, async () => {
+app.listen(PORT, '0.0.0.0', async () => {
   console.log(`🚀 Server running on port ${PORT}`);
   try {
     await prisma.$connect();
